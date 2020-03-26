a99a6f646998d980ec1286e51a8346782dd5a39b
Victor Bo <bvoid@yandex.ru>
2020-03-26 21:36:38 +0200

    200327 v214+
      -wip-

---
+++ buildcrDRom.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#set -e
+
+rund="$(pwd)"
+
+if [ ! "$1" ]; then
+	echo "set build directory"
+	exit
+fi
+
+pushd "$1"
+
+repo init -u https://github.com/crdroidandroid/android.git -b 10.0
+git clone --single-branch https://github.com/eremitein/treble_manifest_crdroid -b v214 .repo/local_manifests
+repo sync -j8 -c --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune
+bash ${rund}/apply-patches.sh ${rund}
+cd device/phh/treble
+bash generate.sh lineage
+cd -
+. build/envsetup.sh
+lunch treble_arm64_bvN-userdebug
+make -j8 systemimage
+
+popd
