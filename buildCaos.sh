30f6a733e6d7737f7db50ba0047a2d9dc1d78310
Victor Bo <bvoid@yandex.ru>
2020-05-29 01:56:06 +0300

    200527 v217

--- buildCaos.sh
+++ buildCaos.sh
@@ -11,7 +11,7 @@ fi
 pushd "$1"
 
 repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r36
-git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v216 .repo/local_manifests
+git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v217 .repo/local_manifests
 repo sync -j8 -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
