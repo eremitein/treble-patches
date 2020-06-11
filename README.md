be2f5bee700b7b2066a414db2a9e56aaf99f123a
Victor Bo <bvoid@yandex.ru>
2020-06-11 05:45:41 +0300

    200611 v219

---
+++ README.md
@@ -0,0 +1,19 @@
+LOSQ Project (LineageOS based)
+
+
+This is GSI project, patches based on the Phh's and Andy Yan works
+
+apply-patches.sh to apply all patches
+	ex. usage:
+	cd /where/los/sources
+	bash ~/files/patches/los/v219/apply-patches.sh ~/files/patches/los/v219/
+
+buildLosq.sh for starting build process from zero
+	ex. usage:
+	cd ~/files/patches/los/v219
+	bash buildLosq.sh /where/los/sources
+
+Patches separated to the three category:
+lineage - specific fixes to adapt LineageOS for GSI (mainly based on Andy's patches)
+generic - Phh's patches, some of them adapted for LineageOS
+personal - my personal additional tweaks & fixes (for LOSQ Project)
--- buildLosq.sh
+++ buildLosq.sh
@@ -2,6 +2,7 @@
 #set -e
 
 rund="$(pwd)"
+para="$(nproc)"
 
 if [ ! "$1" ]; then
 	echo "set build directory"
@@ -11,14 +12,14 @@ fi
 pushd "$1"
 
 repo init -u https://github.com/LineageOS/android.git -b lineage-17.1
-git clone --single-branch https://github.com/eremitein/treble_manifest_losq -b v217 .repo/local_manifests
-repo sync -j8 -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune
+git clone --single-branch https://github.com/eremitein/treble_manifest_losq -b v219 .repo/local_manifests
+repo sync -j${para} -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune ||exit
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
 bash generate.sh lineage
 cd -
 . build/envsetup.sh
 lunch treble_arm64_bvN-userdebug
-make -j8 systemimage
+make -j${para} systemimage
 
 popd
