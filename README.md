5d112770119d1a5c9e713247b32efe3fd3d6f9fe
Victor Bo <bvoid@yandex.ru>
2020-07-16 21:55:10 +0300

    200708 v221

---
+++ README.md
@@ -0,0 +1,15 @@
+CAOS Project (Phh AOSP based)
+
+
+This is GSI project, patches based on the Phh's works
+
+apply-patches.sh to apply all patches
+	ex. usage:
+		cd /where/aosp/sources
+		bash ~/files/patches/aosp/v221/apply-patches.sh ~/files/patches/aosp/v221/
+
+buildCaos.sh for starting build process from zero
+	ex. usage:
+		cd ~/files/patches/aosp/v221
+		bash buildCaos.sh /where/aosp/sources
+
--- buildCaos.sh
+++ buildCaos.sh
@@ -2,6 +2,7 @@
 #set -e
 
 rund="$(pwd)"
+para="$(nproc)"
 
 if [ ! "$1" ]; then
 	echo "set build directory"
@@ -10,15 +11,15 @@ fi
 
 pushd "$1"
 
-repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r36
-git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v217 .repo/local_manifests
-repo sync -j8 -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune
+repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r40
+git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v221 .repo/local_manifests
+repo sync -j${para} -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune ||exit
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
 bash generate.sh
 cd -
 . build/envsetup.sh
 lunch treble_arm64_bvN-userdebug
-make -j8 systemimage
+make -j${para} systemimage
 
 popd
