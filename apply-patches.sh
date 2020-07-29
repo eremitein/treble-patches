4998e43c1909cee231c4587824bd2664effba57b
Victor Bo <bvoid@yandex.ru>
2020-07-29 09:01:48 +0300

    200731 v221
      -wip-

--- apply-patches.sh
+++ apply-patches.sh
@@ -2,34 +2,18 @@
 set -e
 
 patches="$(readlink -f -- $1)"
+assets="crdroid generic personal"
 
-for project in $(cd $patches/patches; echo *); do
-	p="$(tr _ / <<<$project |sed -e 's;platform/;;g')"
-	[ "$p" == build ] && p=build/make
-	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
-	echo
-	pushd $p
-	git clean -fdx; git reset --hard
-	for patch in $patches/patches/$project/[!-]*.patch; do
-		#Check if patch is already applied
-		if patch -f -p1 --dry-run -R -i $patch &> /dev/null; then
-			echo ":: Already applied $patch"
-			continue
-		fi
-
-		if git apply --check $patch; then
-			git am $patch
-		elif patch -f -p1 --dry-run -i $patch &> /dev/null; then
-			#This will fail
-			git am $patch || true
-			patch -f -p1 -i $patch
-			git add -u
-			git am --continue
-			echo ":: Dirty applied $patch"
-		else
-			echo ":: Failed applying $patch"
-		fi
-	done
-	popd
+for d in ${assets}; do
+  for project in $(cd ${patches}/patches/${d}; echo *); do
+    p="$(tr _ / <<<${project} |sed -e 's;platform/;;g')"
+    [ "$p" == build ] && p=build/make
+    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+    echo
+    pushd $p
+      git clean -fdx
+      git reset --hard
+      git am ${patches}/patches/${d}/${project}/0*.patch ||exit
+    popd
+  done
 done
-
--- buildcrDRom.sh
+++ buildcrDRom.sh
@@ -2,23 +2,32 @@
 #set -e
 
 rund="$(pwd)"
+para="$(nproc)"
 
 if [ ! "$1" ]; then
-	echo "set build directory"
-	exit
+  echo "set build directory"
+  exit
 fi
 
 pushd "$1"
 
 repo init -u https://github.com/crdroidandroid/android.git -b 10.0
-git clone --single-branch https://github.com/eremitein/treble_manifest_crdroid -b v215+ .repo/local_manifests
-repo sync -j8 -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune
+git clone --single-branch https://github.com/phhusson/treble_manifest -b android-10.0 .repo/local_manifests
+sed -i -E '/external\/exfat/d' .repo/local_manifests/manifest.xml
+rm .repo/local_manifests/foss.xml
+rm .repo/local_manifests/replace.xml
+rm .repo/local_manifests/gmsexpress.xml
+repo sync -j${para} -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune ||exit
+#mkdir device/generic
+#cd device/generic
+#git clone --single-branch https://android.googlesource.com/device/generic/common
+#cd -
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
 bash generate.sh lineage
 cd -
 . build/envsetup.sh
 lunch treble_arm64_bvN-userdebug
-make -j8 systemimage
+make -j${para} systemimage
 
 popd
