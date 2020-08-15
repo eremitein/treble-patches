965d59a987e082e5d1a1414d5664d13bda92b0f9
Victor Bo <bvoid@yandex.ru>
2020-08-15 13:43:06 -0400

    200815 v222

---
+++ README.md
@@ -0,0 +1,19 @@
+crDRom Project (crDroid based)
+
+
+This is GSI project, patches based on the Phh's and Andy Yan works
+
+apply-patches.sh to apply all patches
+	ex. usage:
+	cd /where/crdroid/sources
+	bash ~/files/patches/crdroid/v222/apply-patches.sh ~/files/patches/crdroid/v222/
+
+buildcrDRom.sh for starting build process from zero
+	ex. usage:
+	cd ~/files/patches/crdroid/v222
+	bash buildcrDRom.sh /where/crdroid/sources
+
+Patches separated to the three category:
+crdroid - specific fixes to adapt crDroid for GSI (mainly based on Andy's patches)
+generic - Phh's patches, some of them adapted for crDroid
+personal - my personal additional tweaks & fixes (for crDRom Project)
--- buildcrDRom.sh
+++ buildcrDRom.sh
@@ -12,16 +12,8 @@ fi
 pushd "$1"
 
 repo init -u https://github.com/crdroidandroid/android.git -b 10.0
-git clone --single-branch https://github.com/phhusson/treble_manifest -b android-10.0 .repo/local_manifests
-sed -i -E '/external\/exfat/d' .repo/local_manifests/manifest.xml
-rm .repo/local_manifests/foss.xml
-rm .repo/local_manifests/replace.xml
-rm .repo/local_manifests/gmsexpress.xml
+git clone --single-branch https://github.com/eremitein/treble_manifest_crdrom -b v222 .repo/local_manifests
 repo sync -j${para} -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune ||exit
-#mkdir device/generic
-#cd device/generic
-#git clone --single-branch https://android.googlesource.com/device/generic/common
-#cd -
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
 bash generate.sh lineage
