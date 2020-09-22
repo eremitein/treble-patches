70674ef24f4e4e809a908d46e95da594443fc583
Victor Bo <bvoid@yandex.ru>
2020-09-22 08:56:34 -0400

    200924 v223

--- apply-patches.sh
+++ apply-patches.sh
@@ -2,15 +2,18 @@
 set -e
 
 patches="$(readlink -f -- $1)"
+assets="spl generic personal"
 
-for project in $(cd ${patches}/patches/; echo *); do
-	p="$(tr _ / <<<${project} |sed -e 's;platform/;;g')"
-	[ "$p" == build ] && p=build/make
-	[ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
-	echo
-	pushd $p
-		git clean -fdx
-		git reset --hard
-		git am ${patches}/patches/${project}/0*.patch ||exit
-	popd
+for d in ${assets}; do
+  for project in $(cd ${patches}/patches/${d}; echo *); do
+    p="$(tr _ / <<<${project} |sed -e 's;platform/;;g')"
+    [ "$p" == build ] && p=build/make
+    [ "$p" == vendor/hardware/overlay ] && p=vendor/hardware_overlay
+    echo
+    pushd $p
+      git clean -fdx
+      git reset --hard
+      git am ${patches}/patches/${d}/${project}/0*.patch ||exit
+    popd
+  done
 done
--- buildCaos.sh
+++ buildCaos.sh
@@ -11,8 +11,8 @@ fi
 
 pushd "$1"
 
-repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r41
-git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v222 .repo/local_manifests
+repo init -u https://android.googlesource.com/platform/manifest -b android-10.0.0_r45
+git clone --single-branch https://github.com/eremitein/treble_manifest_caos -b v223 .repo/local_manifests
 repo sync -j${para} -c -q --force-sync --no-tags --no-clone-bundle --optimized-fetch --prune ||exit
 bash ${rund}/apply-patches.sh ${rund}
 cd device/phh/treble
